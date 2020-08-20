962fc8c40bd194afa8220974d62e2c577ad34958
Jan bjørndalen <jan.bjorndalen@gmail.com>
2020-08-20 13:55:05 +0200

    Changed input type

--- cognito-post-authentication/src/main/java/no/unit/nva/cognito/DebugHandler.java
+++ cognito-post-authentication/src/main/java/no/unit/nva/cognito/DebugHandler.java
@@ -2,15 +2,22 @@ package no.unit.nva.cognito;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Map;
 import nva.commons.utils.JacocoGenerated;
 
 @JacocoGenerated
-public class DebugHandler implements RequestHandler<String, String> {
+public class DebugHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     @Override
     @JacocoGenerated
-    public String handleRequest(String input, Context context) {
-        System.out.println(input);
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        try {
+            new ObjectMapper().writeValue(System.out, input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return input;
     }
 }

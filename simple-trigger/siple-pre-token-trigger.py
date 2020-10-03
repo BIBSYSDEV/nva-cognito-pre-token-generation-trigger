cd3e4276ca58bd52c58f322597989d6328775d8f
svenngjeto <svenn.gjeto@unit.no>
2020-10-03 11:23:08 +0200

    added eventbridge with mapping

--- simple-trigger/siple-pre-token-trigger.py
+++ simple-trigger/siple-pre-token-trigger.py
@@ -1,15 +1,26 @@
-import requests
 import json
+import boto3
+
+client = boto3.client('events')
 
 def lambda_handler(event, context):
-    # TODO implement
-
-    if not (event['request']['userAttributes']['custom:customerId']):
-        print("missing customerId")
-        requests.post("/upsertuser", event)
+    customerId = event.get("request", {}).get("userAttributes", {}).get('custom:customerId')
+    if customerId is None:
+        console.log("missing customerId")
+        eventbridge_put_event(event)
     else:
-        print("has customerid")
-    return {
-        'statusCode': 200,
-        'body': event
-    }
+        console.log("has customerid")
+    return event
+
+def eventbridge_put_event(event):
+    console.log("eventbridge_put_event start")
+    response = client.put_events(
+        Entries=[
+            {
+                'Source': 'nva.cognito',
+                'DetailType': 'updateUserAtributes',
+                'Detail': 'string'
+            },
+        ]
+    )
+    console.log("eventbridge_put_event, message sendt")
